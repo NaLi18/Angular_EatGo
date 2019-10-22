1864b68724f5ea41a7b1a5f9955d10132d457795
NaLi18 <nali@DESKTOP-RP97F9T.localdomain>
2019-10-22 18:50:12 -0400

    Added the get request method.

--- src/app/eat-go-api-service.service.ts
+++ src/app/eat-go-api-service.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EatGoApiServiceService {
-
+  API_KEY:string = "AIzaSyCl1Brlbq3IB2bq5WLX0X7iVMUpG7rLHvg"
   constructor(private httpClient: HttpClient) { }
+
+  public getRestaurant(){
+    return this.httpClient.get<Config>(`https://eatgoapi.org/v2/top-headlines?sources=techcrunch&apiKey=${this.API_KEY}`);
+  }
+}
+
+export interface Config {
+  heroesUrl: string;
+  textfile: string;
 }
