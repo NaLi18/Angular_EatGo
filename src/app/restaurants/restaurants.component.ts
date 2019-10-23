8d90eb49df11f58b7863cc00cb6009790e1ecb61
NaLi18 <nali@DESKTOP-RP97F9T.localdomain>
2019-10-22 20:19:59 -0400

    Link component and view

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { EatGoApiServiceService} from '../eat-go-api-service.service'
 @Component({
   selector: 'app-restaurants',
   templateUrl: './restaurants.component.html',
   styleUrls: ['./restaurants.component.css']
 })
 export class RestaurantsComponent implements OnInit {
-
-  constructor() { }
+  restaurants;
+  constructor(private restaurantApi: EatGoApiServiceService) { }
 
   ngOnInit() {
+    this.restaurantApi.getRestaurant().subscribe((data)=> {
+      console.log(data);
+      this.restaurants = data['resturants'];
+    })
   }
 
 }
